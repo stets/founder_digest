6f6c15d0939f6338a3e121e2f60e868b54baf87d
stets <stetsblake@gmail.com>
2022-12-12 11:15:18 -0500

    began building landing page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,8 +15,10 @@ module.exports = {
         'primary': 'rgb(79, 70, 229)',
         'primary-hover': {
           '700': '#a4411c',
-          '500': '#EA9A72'
-        }
+          '500': '#EA9A72',
+        }, 
+        'accent': '#FF5733', 
+        'accent-hover': '#D2482A'
       }
     },
   },
