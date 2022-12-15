e8e0947bf235e1a4e1dac2e8c7b5f412deff68ad
stets <stetsblake@gmail.com>
2022-12-15 08:15:15 -0500

    bring into sync with frontend - finishing touches, lecture

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
       },
       colors: { // custom color palette for branding
         'primary': 'rgb(79, 70, 229)',
+        'brand-blue': '#18327a',
+        'brand-blue-dark': '#142962',
+        'brand-orange': '#ee9343',
+        'brand-orange-dark': '#c47937',
         'primary-hover': {
           '700': '#a4411c',
           '500': '#EA9A72',
